e1dd2e9508f9504ea448105171ca6bb1562f5d20
Dan MacKinlay <dagithub@danmackinlay.name>
2021-11-20 08:08:16 +1100

    more graphs

--- primitives/src/graphs.py
+++ primitives/src/graphs.py
@@ -50,3 +50,20 @@ def coin_graph_plate(n):
     g.edge('fairness', 'toss')
     return g
 
+
+def ruggedness_graph_plate(n):
+    """
+    Generates a graph of the coin problem, as a plate.
+
+    :param n: The number of coins.
+    :return: A graphviz Digraph object.
+    """
+    g = Digraph(comment='Coin problem')
+    g.attr('node', shape='circle')
+    g.node('fairness')
+    with g.subgraph(name='cluster_1') as c:
+        c.attr(color='black', label=str(n))
+        c.node('toss', style='filled') 
+    g.edge('fairness', 'toss')
+    return g
+
